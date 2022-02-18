01b4eed38cb12febefd9704d9fb6f81fb986bfe5
Sami Fares <djsamif@gmail.com>
2022-02-18 09:53:41 -0500

    starting updates

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,10 @@
 
+// Creating sets for all desired character option sets using arrays //
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
+];
 
 function generatePassword() {
   var passwordStr;
@@ -8,12 +14,12 @@ function generatePassword() {
 
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
